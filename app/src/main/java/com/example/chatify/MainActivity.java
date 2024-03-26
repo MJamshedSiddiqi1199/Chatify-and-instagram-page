fff4d32621f48440d387ab5ec3372c3b857ffb5d
M Jamshed <jamshedxd37@gmail.com>
2024-03-26 20:09:13 +0500

    First commit

--- app/src/main/java/com/example/chatify/MainActivity.java
+++ app/src/main/java/com/example/chatify/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.chatify;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assuming you have a Toolbar with an ID of "toolbar" in your activity_main.xml
+        ImageButton imgBackButton = findViewById(R.id.imgbackbutton);
+        Button backbutton= findViewById(R.id.btnexit);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform your desired operation when the back button is clicked
+                onBackPressed();
+            }
+        });
+        imgBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform your desired operation when the back button is clicked
+                onBackPressed();
+            }
+        });
     }
 }
